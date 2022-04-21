934918d8b8c47f40cd02e1856fdfd8ad49dd4c85
fieryhenry <git@henrythomas.me.uk>
2022-04-21 18:55:29 +0100

    Made clear tutorial also beat korea

--- src/BCSFE_Python/edits/levels/clear_tutorial.py
+++ src/BCSFE_Python/edits/levels/clear_tutorial.py
@@ -1,4 +1,7 @@
 def clear_tutorial(save_stats):
     save_stats["tutorial_cleared"]["Value"] = 1
+    if save_stats["story_chapters"]["Chapter Progress"][0] == 0:
+        save_stats["story_chapters"]["Chapter Progress"][0] = 1
+    save_stats["story_chapters"]["Times Cleared"][0][0] = 1
     print("Successfully cleared the tutorial")
     return save_stats

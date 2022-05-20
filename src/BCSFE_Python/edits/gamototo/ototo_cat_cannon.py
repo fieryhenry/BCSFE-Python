7b9d83684d2bdf5530f9b11169b4294289e9ad52
fieryhenry <git@henrythomas.me.uk>
2022-05-20 19:11:48 +0100

    Added the ability to set specific level of development

--- src/BCSFE_Python/edits/gamototo/ototo_cat_cannon.py
+++ src/BCSFE_Python/edits/gamototo/ototo_cat_cannon.py
@@ -1,16 +1,11 @@
 import helper
 
-def edit_cat_cannon(save_stats):
-    types = ["Base", "Slow Beam", "Iron Wall", "Thunderbolt", "Waterblast", "Holy Blast", "Breakerblast", "Curseblast"]
-    cannons = save_stats["ototo_cannon"]
-    levels = []
-    for cannon in cannons:
-        level = cannons[cannon]["level"]
-        if cannons[cannon]["unlock_flag"] == 0:
-            level -= 1
-        levels.append(level)
-    levels = helper.edit_items_list(types, levels, "Ototo Cat Cannons", [19, 29, 29, 29, 29, 29, 29, 29], offset=1)
+types = ["Base", "Slow Beam", "Iron Wall", "Thunderbolt", "Waterblast", "Holy Blast", "Breakerblast", "Curseblast"]
 
+def set_level(save_stats, levels):
+    cannons = save_stats["ototo_cannon"]
+
+    levels = helper.edit_items_list(types, levels, "Ototo Cat Cannons", [19, 29, 29, 29, 29, 29, 29, 29], offset=1)
     for i in range(len(cannons)):
         cannons[i]["unlock_flag"] = 3
         if levels[i] < 0: cannons[i]["unlock_flag"] = 0
@@ -18,4 +13,31 @@ def edit_cat_cannon(save_stats):
         if levels[i] < 0: levels[i] = 0
         cannons[i]["level"] = levels[i]
     save_stats["ototo_cannon"] = cannons
+    return save_stats
+
+def set_stage(save_stats, stages):
+    cannons = save_stats["ototo_cannon"]
+
+    stages = helper.edit_items_list(types[1:], stages[1:], "Ototo Cat Cannons", 3, type_name="stage")
+    for i in range(len(cannons)-1):
+        cannons[i+1]["unlock_flag"] = stages[i]
+
+    save_stats["ototo_cannon"] = cannons
+    return save_stats
+def edit_cat_cannon(save_stats):
+    cannons = save_stats["ototo_cannon"]
+    levels = []
+    stages = []
+    for cannon in cannons:
+        level = cannons[cannon]["level"]
+        stage = cannons[cannon]["unlock_flag"]
+        if cannons[cannon]["unlock_flag"] == 0:
+            level -= 1
+        levels.append(level)
+        stages.append(stage)
+    stage = helper.valdiate_bool(helper.coloured_text("Do you want to set the level of the cannon &(1)& or the level of construction &(2)& (e.g foundation, style, cannon):", is_input=True), "2")
+    if stage:
+        save_stats = set_stage(save_stats, stages)
+    else:
+        save_stats = set_level(save_stats, levels)
     return save_stats

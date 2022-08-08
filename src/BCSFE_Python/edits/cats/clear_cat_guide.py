3af115ed358000009ec88e7cc9cc508ab0cefd28
fieryhenry <git@henrythomas.me.uk>
2022-08-08 20:12:02 +0100

    Merged gain and remove cat guide into one feature

--- src/BCSFE_Python/edits/cats/clear_cat_guide.py
+++ src/BCSFE_Python/edits/cats/clear_cat_guide.py
@@ -2,27 +2,30 @@
 
 from typing import Any
 
-from ... import helper
+from ... import helper, user_input_handler
 from . import cat_id_selector
 
-def clear_cat_guide(save_stats: dict[str, Any]) -> dict[str, Any]:
+
+def set_cat_guide(save_stats: dict[str, Any]) -> dict[str, Any]:
     """clear cat guide for specific cats"""
-
     ids = cat_id_selector.select_cats(save_stats)
-
-    save_stats = cat_guide_ids(save_stats, ids, 1, "collected")
+    claim = (
+        user_input_handler.colored_input(
+            "Do you want to claim or unclaim the cat guide? (&1&=claim, &2&=unclaim):"
+        )
+        == "1"
+    )
+    if claim:
+        save_stats = cat_guide_ids(save_stats, ids, 1, "collected")
+    else:
+        save_stats = cat_guide_ids(save_stats, ids, 0, "removed")
     return save_stats
 
-def remove_cat_guide(save_stats: dict[str, Any]) -> dict[str, Any]:
-    """clear cat guide for specific cats"""
-
-    ids = cat_id_selector.select_cats(save_stats)
-
-    save_stats = cat_guide_ids(save_stats, ids, 0, "removed")
-    return save_stats
 
 
-def cat_guide_ids(save_stats: dict[str, Any], ids: list[int], val: int, string: str) -> dict[str, Any]:
+def cat_guide_ids(
+    save_stats: dict[str, Any], ids: list[int], val: int, string: str
+) -> dict[str, Any]:
     """Clear cat guide for a set of cat ids"""
     ids = helper.check_cat_ids(ids, save_stats)
     cat_guide_collected = save_stats["cat_guide_collected"]

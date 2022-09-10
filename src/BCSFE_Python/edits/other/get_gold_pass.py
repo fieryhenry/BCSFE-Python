2240bf6833fe7a593363aa2e8b3857dc8e84e6e7
fieryhenry <git@henrythomas.me.uk>
2022-09-10 20:06:27 +0100

    Changed get gold pass to also wipe catfood login bonuses and to get the paid version instead

--- src/BCSFE_Python/edits/other/get_gold_pass.py
+++ src/BCSFE_Python/edits/other/get_gold_pass.py
@@ -11,19 +11,30 @@ def get_gold_pass_val(save_stats: dict[str, Any], total_days: int) -> dict[str, 
 
     gold_pass = save_stats["gold_pass"]
 
-    start_date = time.time()
+    start_date = int(time.time())
     expiry_date = start_date + datetime.timedelta(days=total_days).total_seconds()
+    expiry_date_2 = start_date + datetime.timedelta(days=total_days * 2).total_seconds()
+
     gold_pass["officer_id"]["Value"] = 1
-    gold_pass["flag"]["Value"] = 1
+    if gold_pass["renewal_times"]["Value"] == 0:
+        gold_pass["renewal_times"]["Value"] = 1
+    gold_pass["renewal_times"]["Value"] += 1
     gold_pass["start_date"] = start_date
     gold_pass["expiry_date"] = expiry_date
+    gold_pass["unknown_2"][0] = expiry_date
+    gold_pass["unknown_2"][1] = expiry_date_2
+
     gold_pass["start_date_2"] = start_date
-    gold_pass["expiry_date_2"] = expiry_date
+    gold_pass["expiry_date_2"] = expiry_date_2
     gold_pass["unknown_3"] = start_date
-    gold_pass["flag_2"]["Value"] = 1
+    gold_pass["flag_2"]["Value"] = 2
     gold_pass["expiry_date_3"] = expiry_date
 
+    gold_pass["unknown_4"]["Value"] = 0
+    gold_pass["unknown_5"]["Value"] = 1
+    gold_pass["unknown_6"]["Value"] = 0
     save_stats["gold_pass"] = gold_pass
+    save_stats["login_bonuses"][5100] = 0
 
     return save_stats
 

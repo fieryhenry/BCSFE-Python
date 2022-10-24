e6f27f876915d5cf307e023f65c4886c4940e47c
fieryhenry <github@henrythomas.me.uk>
2022-10-24 17:14:38 +0100

    Changed aku to work differently enter the progress you want to clear up to and including

--- src/BCSFE_Python/edits/levels/aku.py
+++ src/BCSFE_Python/edits/levels/aku.py
@@ -1,36 +1,31 @@
 """Handler for editing the aku realm"""
 from typing import Any
 from . import story_level_id_selector, unlock_aku_realm
-from ... import helper, user_input_handler
+from ... import helper
 
 
 def edit_aku(save_stats: dict[str, Any]) -> dict[str, Any]:
     """Clear whole chapters"""
 
     save_stats = unlock_aku_realm.unlock_aku_realm(save_stats)
-    options = ["Clear aku stages", "Wipe aku data"]
-    option = user_input_handler.select_single(options, "select")
-    if option == 2:
-        return wipe_aku(save_stats)
 
     aku = save_stats["aku"]["Value"]
 
-    ids = story_level_id_selector.select_levels(None, total=49)
-    aku["clear_progress"][0][0] = max(ids)
+    progress = story_level_id_selector.select_level_progress(None, total=49)
+    progress = helper.clamp(progress, 0, 49)
+    if progress == 0:
+        aku["clear_progress"][0][0] = 0
+        aku["clear_amount"][0][0] = [0] * len(aku["clear_amount"][0][0])
 
-    for level_id in ids:
-        aku["clear_amount"][0][0][level_id] = 1
+    else:
+        stage_index = progress - 1
+        aku["clear_progress"][0][0] = min(progress, 48)
+        aku["clear_amount"][0][0][stage_index] = 1
+        for i in range(stage_index):
+            aku["clear_amount"][0][0][i] = 1
+        for i in range(stage_index + 1, 49):
+            aku["clear_amount"][0][0][i] = 0
+
     save_stats["aku"]["Value"] = aku
-    helper.colored_text("Successfully cleared aku stages")
-    return save_stats
-
-
-def wipe_aku(save_stats: dict[str, Any]) -> dict[str, Any]:
-    """Reset aku data"""
-
-    aku = save_stats["aku"]["Value"]
-    aku["clear_progress"][0][0] = 0
-    aku["clear_amount"][0][0] = [0] * len(aku["clear_amount"][0][0])
-    save_stats["aku"]["Value"] = aku
-    helper.colored_text("Successfully wiped aku data")
+    helper.colored_text("Successfully set aku stages")
     return save_stats

afc531d69d00db63dda8c5686ae4822fa74b1716
fieryhenry <github@henrythomas.me.uk>
2022-11-05 10:58:43 +0000

    Fixed outbreaks not setting all stages

--- src/BCSFE_Python/edits/levels/outbreaks.py
+++ src/BCSFE_Python/edits/levels/outbreaks.py
@@ -1,5 +1,5 @@
 """Handler for editting outbreaks"""
-from typing import Any
+from typing import Any, Optional
 
 from ... import user_input_handler, helper
 from . import main_story
@@ -18,28 +18,36 @@ def get_available_chapters(outbreaks: dict[int, Any]) -> list[str]:
     return available_chapters
 
 
-def set_outbreak(chapter_data: dict[int, int], val_to_set: int) -> dict[int, int]:
+def set_outbreak(
+    chapter_data: dict[int, int], val_to_set: int, total: Optional[int] = None
+) -> dict[int, int]:
     """Set a chapter of an outbreak"""
+    if total is None:
+        total = len(chapter_data)
 
-    for level_id in chapter_data.keys():
+    for level_id in range(total):
         chapter_data[level_id] = val_to_set
     return chapter_data
 
 
 def set_outbreaks(
-    outbreaks: dict[int, Any],
+    outbreaks: dict[str, Any],
     current_outbreaks: dict[int, Any],
     ids: list[int],
     clear: bool = True,
-) -> tuple[dict[int, Any], dict[int, Any]]:
+) -> tuple[dict[str, Any], dict[int, Any]]:
     """Set outbreaks"""
-
+    chapter_data = outbreaks["outbreaks"]
     for chapter_id in ids:
-        outbreaks[chapter_id] = set_outbreak(outbreaks[chapter_id], 1 if clear else 0)
+        outbreaks["stages_counts"][chapter_id] = 48
+        chapter_data[chapter_id] = set_outbreak(
+            chapter_data[chapter_id], 1 if clear else 0, 48
+        )
         if chapter_id in current_outbreaks:
             current_outbreaks[chapter_id] = set_outbreak(
                 current_outbreaks[chapter_id], 0
             )
+    outbreaks["outbreaks"] = chapter_data
     return outbreaks, current_outbreaks
 
 
@@ -66,9 +74,9 @@ def edit_outbreaks(save_stats: dict[str, Any]) -> dict[str, Any]:
     ids = helper.check_clamp(ids, len(available_chapters) + 1, 0, 0)
     ids = main_story.format_story_ids(ids)
     outbreaks, current_outbreaks = set_outbreaks(
-        outbreaks, current_outbreaks, ids, clear
+        save_stats["outbreaks"], current_outbreaks, ids, clear
     )
-    save_stats["outbreaks"]["outbreaks"] = outbreaks
+    save_stats["outbreaks"] = outbreaks
     save_stats["current_outbreaks"]["outbreaks"] = current_outbreaks
     print("Successfully set outbreaks")
     return save_stats

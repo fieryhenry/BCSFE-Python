43b660b4a6d5eab17cff8187633e3427c20ee3f7
fieryhenry <github@henrythomas.me.uk>
2022-12-09 17:34:49 +0000

    Fixed

--- src/BCSFE_Python/root_handler.py
+++ src/BCSFE_Python/root_handler.py
@@ -85,6 +85,6 @@ def rerun_game(version: str) -> None:
         return
     package_name = "jp.co.ponos.battlecats" + version.replace("jp", "")
     # close game locally, NOT using adb
-    subprocess.run(f"sudo pkill -f {package_name}", capture_output=True, check=False)
+    subprocess.run(f"sudo pkill -f {package_name}", capture_output=True, check=False, shell=True)
     # open game locally, NOT using adb
-    subprocess.run(f"sudo monkey -p {package_name} -c android.intent.category.LAUNCHER 1", capture_output=True, check=False)
+    subprocess.run(f"sudo monkey -p {package_name} -c android.intent.category.LAUNCHER 1", capture_output=True, check=False, shell=True)

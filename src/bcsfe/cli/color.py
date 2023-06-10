67709d2674d3ed1fe1c2072c06b33776733945ed
fieryhenry <github@henrythomas.me.uk>
2023-06-10 18:38:50 +0100

    improved colors

--- src/bcsfe/cli/color.py
+++ src/bcsfe/cli/color.py
@@ -1,30 +1,48 @@
-import enum
 from typing import Any
+from aenum import NamedConstant  # type: ignore
 import colored  # type: ignore
 from bcsfe.core import locale_handler
 
 
-class ColorHex(enum.Enum):
+class ColorHex(NamedConstant):
     GREEN = "#008000"
+    G = GREEN
     RED = "#FF0000"
+    R = RED
     DARK_YELLOW = "#D7C32A"
+    DY = DARK_YELLOW
     BLACK = "#000000"
+    BL = BLACK
     WHITE = "#FFFFFF"
+    W = WHITE
     CYAN = "#00FFFF"
+    C = CYAN
     DARK_GREY = "#A9A9A9"
+    DG = DARK_GREY
     BLUE = "#0000FF"
+    B = BLUE
     YELLOW = "#FFFF00"
+    Y = YELLOW
     MAGENTA = "#FF00FF"
+    M = MAGENTA
     DARK_BLUE = "#00008B"
+    DB = DARK_BLUE
     DARK_CYAN = "#008B8B"
+    DC = DARK_CYAN
     DARK_MAGENTA = "#8B008B"
+    DM = DARK_MAGENTA
     DARK_RED = "#8B0000"
+    DR = DARK_RED
     DARK_GREEN = "#006400"
+    DGN = DARK_GREEN
     LIGHT_GREY = "#D3D3D3"
+    LG = LIGHT_GREY
 
     @staticmethod
     def from_name(name: str) -> str:
-        return ColorHex[name.upper()].value
+        if name == "":
+            return ""
+        return getattr(ColorHex, name.upper())
 
 
 class ColoredText:
@@ -50,10 +68,10 @@ class ColoredText:
                 print(colored.stylize(text, fg), end="")  # type: ignore
 
     @staticmethod
-    def localize(string: str, perams: tuple[Any, ...]) -> "ColoredText":
+    def localize(string: str, **kwargs: Any) -> "ColoredText":
         text = locale_handler.LocalManager().get_key(string)
         try:
-            text = text % perams
+            text = text.format(**kwargs)
         except TypeError:
             pass
         return ColoredText(text)
@@ -69,8 +87,7 @@ class ColoredText:
         # output: [("This is a ", "#FF0000"), ("white", "#FFFFFF"), (" red text", "#FF0000")]
 
         # allow escaping of < and > with \, so that \\<red\\> is not parsed as a color tag
-        if not txt.endswith(">"):
-            txt += "</>"
+        txt += "</>"
         output: list[tuple[str, str]] = []
         i = 0
         tags: list[str] = []
@@ -122,12 +139,12 @@ class ColoredText:
         return output
 
 
-class ColoredInput(ColoredText):
+class ColoredInput:
     def __init__(self, end: str = "") -> None:
-        super().__init__(end)
+        self.end = end
 
     def get(self, display_string: str) -> str:
-        self.display(display_string)
+        ColoredText(display_string, end=self.end)
         return input()
 
     def get_int(
@@ -139,7 +156,7 @@ class ColoredInput(ColoredText):
             try:
                 return int(self.get(display_string))
             except ValueError:
-                self.display(error_message)
+                ColoredText(error_message)
 
     def get_bool(
         self,

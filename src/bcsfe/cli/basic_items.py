a30c7cd14a6f2c02269cedf44c23446f18400278
fieryhenry <github@henrythomas.me.uk>
2023-06-15 10:56:19 +0100

    static method

--- src/bcsfe/cli/basic_items.py
+++ src/bcsfe/cli/basic_items.py
@@ -3,155 +3,159 @@ from bcsfe.cli import dialog_creator, color
 
 
 class BasicItems:
-    def __init__(self, save_file: "io.save.SaveFile"):
-        self.save_file = save_file
-
-    def edit_catfood(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(22)
-        original_amount = self.save_file.catfood
-        self.save_file.catfood = dialog_creator.SingleEditor(
-            name, self.save_file.catfood, 45000
+    @staticmethod
+    def edit_catfood(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(22)
+        original_amount = save_file.catfood
+        save_file.catfood = dialog_creator.SingleEditor(
+            name, save_file.catfood, 45000
         ).edit()
-        change = self.save_file.catfood - original_amount
-        server.managed_item.BackupMetaData(self.save_file).add_managed_item(
+        change = save_file.catfood - original_amount
+        server.managed_item.BackupMetaData(save_file).add_managed_item(
             server.managed_item.ManagedItem.from_change(
                 change, server.managed_item.ManagedItemType.CATFOOD
             )
         )
 
-    def edit_xp(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(6)
-        self.save_file.xp = dialog_creator.SingleEditor(
-            name, self.save_file.xp, 99999999
+    @staticmethod
+    def edit_xp(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(6)
+        save_file.xp = dialog_creator.SingleEditor(name, save_file.xp, 99999999).edit()
+
+    @staticmethod
+    def edit_normal_tickets(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(20)
+        save_file.normal_tickets = dialog_creator.SingleEditor(
+            name, save_file.normal_tickets, 2999
         ).edit()
 
-    def edit_normal_tickets(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(20)
-        self.save_file.normal_tickets = dialog_creator.SingleEditor(
-            name, self.save_file.normal_tickets, 2999
+    @staticmethod
+    def edit_rare_tickets(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(21)
+        original_amount = save_file.rare_tickets
+        save_file.rare_tickets = dialog_creator.SingleEditor(
+            name, save_file.rare_tickets, 299
         ).edit()
-
-    def edit_rare_tickets(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(21)
-        original_amount = self.save_file.rare_tickets
-        self.save_file.rare_tickets = dialog_creator.SingleEditor(
-            name, self.save_file.rare_tickets, 299
-        ).edit()
-        change = self.save_file.rare_tickets - original_amount
-        server.managed_item.BackupMetaData(self.save_file).add_managed_item(
+        change = save_file.rare_tickets - original_amount
+        server.managed_item.BackupMetaData(save_file).add_managed_item(
             server.managed_item.ManagedItem.from_change(
                 change, server.managed_item.ManagedItemType.RARE_TICKET
             )
         )
 
-    def edit_platinum_tickets(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(29)
-        original_amount = self.save_file.platinum_tickets
-        self.save_file.platinum_tickets = dialog_creator.SingleEditor(
-            name, self.save_file.platinum_tickets, 9
+    @staticmethod
+    def edit_platinum_tickets(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(29)
+        original_amount = save_file.platinum_tickets
+        save_file.platinum_tickets = dialog_creator.SingleEditor(
+            name, save_file.platinum_tickets, 9
         ).edit()
-        change = self.save_file.platinum_tickets - original_amount
-        server.managed_item.BackupMetaData(self.save_file).add_managed_item(
+        change = save_file.platinum_tickets - original_amount
+        server.managed_item.BackupMetaData(save_file).add_managed_item(
             server.managed_item.ManagedItem.from_change(
                 change, server.managed_item.ManagedItemType.PLATINUM_TICKET
             )
         )
 
-    def edit_legend_tickets(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(145)
-        original_amount = self.save_file.legend_tickets
-        self.save_file.legend_tickets = dialog_creator.SingleEditor(
-            name, self.save_file.legend_tickets, 4
+    @staticmethod
+    def edit_legend_tickets(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(145)
+        original_amount = save_file.legend_tickets
+        save_file.legend_tickets = dialog_creator.SingleEditor(
+            name, save_file.legend_tickets, 4
         ).edit()
-        change = self.save_file.legend_tickets - original_amount
-        server.managed_item.BackupMetaData(self.save_file).add_managed_item(
+        change = save_file.legend_tickets - original_amount
+        server.managed_item.BackupMetaData(save_file).add_managed_item(
             server.managed_item.ManagedItem.from_change(
                 change, server.managed_item.ManagedItemType.LEGEND_TICKET
             )
         )
 
-    def edit_platinum_shards(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(157)
-        platinum_ticket_amount = self.save_file.platinum_tickets
+    @staticmethod
+    def edit_platinum_shards(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(157)
+        platinum_ticket_amount = save_file.platinum_tickets
         max_value = 99 - (platinum_ticket_amount * 10)
-        self.save_file.platinum_shards = dialog_creator.SingleEditor(
-            name, self.save_file.platinum_shards, max_value
+        save_file.platinum_shards = dialog_creator.SingleEditor(
+            name, save_file.platinum_shards, max_value
         ).edit()
 
-    def edit_np(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(7)
-        self.save_file.np = dialog_creator.SingleEditor(
-            name, self.save_file.np, 9999
+    @staticmethod
+    def edit_np(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(7)
+        save_file.np = dialog_creator.SingleEditor(name, save_file.np, 9999).edit()
+
+    @staticmethod
+    def edit_leadership(save_file: "io.save.SaveFile"):
+        name = game.catbase.gatya_item.GatyaItemNames(save_file.cc).get_name(105)
+        save_file.leadership = dialog_creator.SingleEditor(
+            name, save_file.leadership, 9999
         ).edit()
 
-    def edit_leadership(self):
-        name = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).get_name(105)
-        self.save_file.leadership = dialog_creator.SingleEditor(
-            name, self.save_file.leadership, 9999
-        ).edit()
-
-    def edit_catamins(self):
-        names = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).names
-        items = game.catbase.gatya_item.GatyaItemBuy(self.save_file.cc).get_by_category(
-            6
-        )
+    @staticmethod
+    def edit_catamins(save_file: "io.save.SaveFile"):
+        names = game.catbase.gatya_item.GatyaItemNames(save_file.cc).names
+        items = game.catbase.gatya_item.GatyaItemBuy(save_file.cc).get_by_category(6)
         names = [names[item.id] for item in items]
         values = dialog_creator.MultiEditor.from_reduced(
             "catamins",
             names,
-            self.save_file.catamins,
+            save_file.catamins,
             9999,
             group_name_localized=True,
         ).edit()
-        self.save_file.catamins = values
+        save_file.catamins = values
 
-    def edit_catseyes(self):
-        names = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).names
-        items = game.catbase.gatya_item.GatyaItemBuy(self.save_file.cc).get_by_category(
-            5
-        )
+    @staticmethod
+    def edit_catseyes(save_file: "io.save.SaveFile"):
+        names = game.catbase.gatya_item.GatyaItemNames(save_file.cc).names
+        items = game.catbase.gatya_item.GatyaItemBuy(save_file.cc).get_by_category(5)
         names = [names[item.id] for item in items]
         values = dialog_creator.MultiEditor.from_reduced(
             "catseyes",
             names,
-            self.save_file.catseyes,
+            save_file.catseyes,
             9999,
             group_name_localized=True,
         ).edit()
-        self.save_file.catseyes = values
+        save_file.catseyes = values
 
-    def edit_catfruit(self):
-        names = game.catbase.matatabi.Matatabi(self.save_file.cc).get_names()
+    @staticmethod
+    def edit_catfruit(save_file: "io.save.SaveFile"):
+        names = game.catbase.matatabi.Matatabi(save_file.cc).get_names()
 
         max_value = 998
-        if self.save_file.game_version < 110400:
+        if save_file.game_version < 110400:
             max_value = 128
 
         values = dialog_creator.MultiEditor.from_reduced(
             "catfruit",
             names,
-            self.save_file.catfruit,
+            save_file.catfruit,
             max_value,
             group_name_localized=True,
             cumulative_max=True,
         ).edit()
-        self.save_file.catfruit = values
+        save_file.catfruit = values
 
-    def set_restart_pack(self):
-        self.save_file.restart_pack = 1
-        names = game.catbase.gatya_item.GatyaItemNames(self.save_file.cc).names
+    @staticmethod
+    def set_restart_pack(save_file: "io.save.SaveFile"):
+        save_file.restart_pack = 1
+        names = game.catbase.gatya_item.GatyaItemNames(save_file.cc).names
         name = names[123]
         color.ColoredText.localize("value_gave", name=name)
 
-    def edit_inquiry_code(self):
+    @staticmethod
+    def edit_inquiry_code(save_file: "io.save.SaveFile"):
         item_name = (
-            game.localizable.Localizable(self.save_file.cc).get("autoSave_txt5").strip()
+            game.localizable.Localizable(save_file.cc).get("autoSave_txt5").strip()
         )
-        self.save_file.inquiry_code = dialog_creator.StringEditor(
-            item_name, self.save_file.inquiry_code
+        save_file.inquiry_code = dialog_creator.StringEditor(
+            item_name, save_file.inquiry_code
         ).edit()
 
-    def edit_password_refresh_token(self):
-        self.save_file.password_refresh_token = dialog_creator.StringEditor(
-            "password_refresh_token", self.save_file.password_refresh_token
+    @staticmethod
+    def edit_password_refresh_token(save_file: "io.save.SaveFile"):
+        save_file.password_refresh_token = dialog_creator.StringEditor(
+            "password_refresh_token", save_file.password_refresh_token
         ).edit()

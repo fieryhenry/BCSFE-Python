f60036359b48029c26abbd46621c680e71277966
fieryhenry <github@henrythomas.me.uk>
2023-06-17 11:56:49 +0100

    made optional

--- src/bcsfe/cli/file_dialog.py
+++ src/bcsfe/cli/file_dialog.py
@@ -1,6 +1,7 @@
 from tkinter import filedialog
 import tkinter as tk
 from bcsfe.core import locale_handler
+from typing import Optional
 
 
 class FileDialog:
@@ -12,7 +13,7 @@ class FileDialog:
     def get_file(
         self,
         title: str,
-        filetypes: list[tuple[str, str]] = [],
+        filetypes: Optional[list[tuple[str, str]]] = None,
         initialdir: str = "",
         initialfile: str = "",
     ) -> str:
@@ -31,14 +32,28 @@ class FileDialog:
     def save_file(
         self,
         title: str,
-        filetypes: list[tuple[str, str]] = [],
+        filetypes: Optional[list[tuple[str, str]]] = None,
         initialdir: str = "",
         initialfile: str = "",
-    ) -> str:
+    ) -> Optional[str]:
+        """Save file dialog
+
+        Args:
+            title (str): Title of dialog.
+            filetypes (Optional[list[tuple[str, str]]], optional): File types. Defaults to None.
+            initialdir (str, optional): Initial directory. Defaults to "".
+            initialfile (str, optional): Initial file. Defaults to "".
+
+        Returns:
+            Optional[str]: Path to file.
+        """
         title = locale_handler.LocalManager().get_key(title)
-        return filedialog.asksaveasfilename(
-            title=title,
-            filetypes=filetypes,
-            initialdir=initialdir,
-            initialfile=initialfile,
+        return (
+            filedialog.asksaveasfilename(
+                title=title,
+                filetypes=filetypes,
+                initialdir=initialdir,
+                initialfile=initialfile,
+            )
+            or None
         )

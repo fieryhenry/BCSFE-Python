71837f1ccddf7794c2e1ac62ed9ba175421e46e6
fieryhenry <github@henrythomas.me.uk>
2023-11-11 12:52:52 +0000

    managed items + rerun

--- src/bcsfe/scripting/save_save.py
+++ src/bcsfe/scripting/save_save.py
@@ -12,10 +12,55 @@ class SaveSaveParser:
         method = scripting.handle_string_field(self.data, "method")
         if method is None:
             raise scripting.ParsingError("Missing method")
+
+        managed_items = self.data.get("managed-items")
+        if managed_items:
+            self.handle_managed_items(managed_items)
+
         if method == "adb-push":
             AdbPushSave(self.data).save()
             return
         raise scripting.ParsingError(f"Unknown method: {method}")
+
+    def handle_managed_items(self, managed_items: dict[str, Any]):
+        enabled = scripting.handle_bool_field(managed_items, "enabled")
+        if enabled is None:
+            enabled = True
+        should_print = scripting.handle_bool_field(managed_items, "print")
+        if should_print is None:
+            should_print = False
+        allow_iq_change = scripting.handle_bool_field(managed_items, "allow-iq-change")
+        if allow_iq_change is None:
+            allow_iq_change = False
+
+        if enabled:
+            loop_count = 0
+            while True:
+                save_managed_items = core.BackupMetaData(
+                    scripting.context.get_save()
+                ).get_managed_items()
+                if save_managed_items:
+                    server_handler = core.ServerHandler(
+                        scripting.context.get_save(), should_print
+                    )
+                    success = server_handler.upload_meta_data()
+                    if not success:
+                        if not allow_iq_change:
+                            raise scripting.ParsingError(
+                                core.core_data.local_manager.get_key(
+                                    "s!_failed_to_upload_meta"
+                                )
+                            )
+                        server_handler.create_new_account()
+                        loop_count += 1
+                        if loop_count > 1:
+                            raise scripting.ParsingError(
+                                core.core_data.local_manager.get_key(
+                                    "s!_failed_to_upload_meta"
+                                )
+                            )
+                        continue
+                break
 
 
 class SaveSaveParserBase:
@@ -37,9 +82,13 @@ class AdbPushSave(SaveSaveParserBase):
         else:
             cc = core.CountryCode.from_code(cc)
 
+        rerun = scripting.handle_bool_field(self.data, "rerun")
+        if rerun is None:
+            rerun = False
+
         adb_handler = core.AdbHandler()
         adb_handler.set_device(device)
         adb_handler.set_cc(cc)
-        result = adb_handler.load_save(scripting.context.get_save())
+        result = adb_handler.load_save(scripting.context.get_save(), rerun)
         if not result.success:
             raise scripting.ParsingError(f"Failed to save locally: {result.result}")

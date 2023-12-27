8053c243fdc7d67cfa3b021c8e3fe0d5cff3068c
Pulse <72387902+cintagram@users.noreply.github.com>
2023-12-27 13:52:08 +0900

    zerolegends.py 업데이트

--- src/BCSFE_Python/edits/levels/zerolegends.py
+++ src/BCSFE_Python/edits/levels/zerolegends.py
@@ -9,18 +9,20 @@ def count_chapters(save_stats) -> int:
     count = len(data1)
     return count
     
+def set_zl(stage_data, ids, lengths):
+    
 
 def edit_zl(save_stats: dict[str, Any]) -> dict[str, Any]:
     """Handler for editting zero legends"""
     stage_data = save_stats["zero_legends"]
-
+    lengths = count_chapters(save_stats)
     ids = []
     ids = user_input_handler.get_range(
         user_input_handler.colored_input(
             "Enter stage ids (e.g &1& = Zero Field, &2& = The Edge of Spacetime)(You can enter &all& to get all, a range e.g &1&-&8&, or ids separate by spaces e.g &5 4 7&):"
         ),
-        count_chapters(save_stats),
+        lengths,
     )
-    save_stats["zero_legends"] = event_stages.stage_handler(stage_data, ids, -1)
+    stage_data = set_zl(stage_data, ids, lengths)
 
     return save_stats

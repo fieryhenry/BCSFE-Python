16d00d772b432f48da21066296108c798ef92aaa
fieryhenry <dev@henrythomas.me.uk>
2024-10-19 10:27:26 +0100

    add bcsfe as a cmdline script

--- src/bcsfe/__init__.py
+++ src/bcsfe/__init__.py
@@ -4,3 +4,9 @@ from bcsfe import core, cli
 
 
 __all__ = ["core", "cli"]
+
+
+def run():
+    from bcsfe import __main__
+
+    __main__.main()
--- src/bcsfe/__main__.py
+++ src/bcsfe/__main__.py
@@ -7,22 +7,27 @@ import sys
 
 import bcsfe
 
-args = sys.argv[1:]
-for arg in args:
-    if arg.lower() in ["--version", "-v"]:
-        print(bcsfe.__version__)
-        exit()
 
-try:
-    cli.main.Main().main()
-except KeyboardInterrupt:
-    cli.main.Main.leave()
-except Exception as e:
-    tb = traceback.format_exc()
-    cli.color.ColoredText.localize("error", error=e, traceback=tb)
+def main():
+    args = sys.argv[1:]
+    for arg in args:
+        if arg.lower() in ["--version", "-v"]:
+            print(bcsfe.__version__)
+            exit()
+
     try:
-        cli.main.Main.exit_editor()
-    except Exception:
-        pass
+        cli.main.Main().main()
     except KeyboardInterrupt:
-        pass
+        cli.main.Main.leave()
+    except Exception as e:
+        tb = traceback.format_exc()
+        cli.color.ColoredText.localize("error", error=e, traceback=tb)
+        try:
+            cli.main.Main.exit_editor()
+        except Exception:
+            pass
+        except KeyboardInterrupt:
+            pass
+
+
+main()

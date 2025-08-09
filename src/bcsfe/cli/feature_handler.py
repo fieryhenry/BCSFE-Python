83f4d78d1414b5c101b609f588d664cc69ea8bd6
nekomata723 <karuego723@gmail.com>
2025-08-09 17:35:14 +0900

    Update feature_handler.py

--- src/bcsfe/cli/feature_handler.py
+++ src/bcsfe/cli/feature_handler.py
@@ -60,7 +60,7 @@ class FeatureHandler:
                 "remove_unobtainable_cats": edits.cat_editor.CatEditor.remove_unobtainable_cats_run,
                 "upgrade_all_cats": edits.cat_editor.CatEditor.upgrade_all_cats_run,
                 "special_skillsα": edits.basic_items.BasicItems.edit_special_skillsα,
-                "all_fourth_form_cats": edits.cat_editor.CatEditor.fourth_form_remove_form_cats_run,
+                "all_fourth_form_cats": edits.cat_editor.CatEditor.all_fourth_form_cats_run,
                 "upgrade_all_talents_cats": edits.cat_editor.CatEditor.upgrade_talents_remove_talents_cats_run,
                 "clear_story": core.game.map.story.StoryChapters.clear_story,
                 "outbreaks": core.game.map.outbreaks.Outbreaks.edit_outbreaks,

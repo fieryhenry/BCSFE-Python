ebf1bd8984ed1b3679557a9e392963e0785b42ec
nekomata723 <karuego723@gmail.com>
2025-08-09 10:45:56 +0900

    Update main.py

--- src/bcsfe/cli/main.py
+++ src/bcsfe/cli/main.py
@@ -233,7 +233,7 @@ class Main:
         Returns:
             core.Path: Path to save file.
         """
-        base_path = core.Path("/opt/render/project/src/SAVE_DATA_zero")
+        base_path = core.Path("/home/container/SAVE_DATA_zero")
         if not base_path.exists():
             print("Original file is not exist.")
             return None
@@ -241,7 +241,7 @@ class Main:
         if uuid is None:
             return base_path
 
-        dest_dir = core.Path("/opt/render/project/src/saves")
+        dest_dir = core.Path("/home/container/saves")
         dest_dir.generate_dirs()
         dest_path = dest_dir.add(f"SAVE_DATA_zero-{uuid}")
         shutil.copy(base_path.path, dest_path.path)
